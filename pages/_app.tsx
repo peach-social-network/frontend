38eae9e9b0ef4eeecc786e17bf20777a4ac83d4b
Vapoltavecs <vapoltavecs@gmail.com>
2022-12-10 00:15:51 +0400

    feat: F-5 | add plugin

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,3 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -8,3 +7,4 @@ function MyApp({ Component, pageProps }: AppProps) {
 }
 
 export default MyApp
+ 
